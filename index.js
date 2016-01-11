239c4ce4cc52500f75eb478b67730a838288afc4
case <case@casesandberg.com>
2016-01-11 13:43:51 -0600

    Handle skipping `excludeFiles`
    https://github.com/AtomLinter/linter-jscs/issues/134

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import path from 'path';
 import configFile from 'jscs/lib/cli-config';
+import globule from 'globule';
 
 const grammarScopes = ['source.js', 'source.js.jsx'];
 
@@ -74,9 +75,16 @@ export default class LinterJSCS {
     require('atom-package-deps').install();
 
     this.observer = atom.workspace.observeTextEditors((editor) => {
-      editor.getBuffer().onDidSave(() => {
-        if (grammarScopes.indexOf(editor.getGrammar().scopeName) !== -1 && this.fixOnSave) {
-          this.fixString(editor);
+      editor.getBuffer().onWillSave(() => {
+
+        // Exclude `excludeFiles` for fix on save
+        const config = this.getConfig(editor.getPath());
+        var exclude = globule.isMatch(config && config.excludeFiles, this.getFilePath(editor.getPath()));
+
+        if (grammarScopes.indexOf(editor.getGrammar().scopeName) !== -1 && this.fixOnSave && !exclude) {
+          process.nextTick(() => {
+            this.fixString();
+          });
         }
       });
     });
@@ -102,8 +110,7 @@ export default class LinterJSCS {
         this.jscs.registerDefaultRules();
 
         const filePath = editor.getPath();
-        const config = configFile.load(false,
-          path.join(path.dirname(filePath), this.configPath));
+        const config = this.getConfig(filePath);
 
         // Options passed to `jscs` from package configuration
         const options = { esnext: this.esnext, preset: this.preset };
@@ -118,6 +125,10 @@ export default class LinterJSCS {
         const errors = this.jscs
           .checkString(text, filePath)
           .getErrorList();
+
+        // Exclude `excludeFiles` for errors
+        var exclude = globule.isMatch(config && config.excludeFiles, this.getFilePath(editor.getPath()));
+        if (exclude) return [];
 
         return errors.map(({ rule, message, line, column }) => {
 
@@ -137,18 +148,28 @@ export default class LinterJSCS {
     };
   }
 
+  static getFilePath(path) {
+    const relative = atom.project.relativizePath(path);
+    return relative[1];
+  }
+
+  static getConfig(filePath) {
+    return configFile.load(false,
+      path.join(path.dirname(filePath), this.configPath));
+  }
+
   static fixString(editor) {
     const editorPath = editor.getPath();
     const editorText = editor.getText();
 
-    const config = configFile.load(false,path.join(path.dirname(editorPath), this.configPath));
+    const config = this.getConfig(editorPath);
     if (!config && this.onlyConfig) {
-      return
+      return;
     }
 
     const fixedText = this.jscs.fixString(editorText, editorPath).output;
     if (editorText === fixedText) {
-      return
+      return;
     }
 
     const cursorPosition = editor.getCursorScreenPosition();
